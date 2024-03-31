3e7619d8da4b43404059a608eb0698bfb897883c
OjeeAfolabi <afolabiojee@gmail.com>
2024-03-31 20:54:15 +0100

    created items in the cart

---
+++ script.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    id: 0,
+    image: "images/sweatshirt.webp",
+    title: "Black Longsleeve Men",
+    price: 25,
+  },
+
+  {
+    id: 1,
+    image: "images/justdoit.webp",
+    title: "Grey Longsleeve Men",
+    price: 32,
+  },
+
+  {
+    id: 2,
+    image: "images/ashlong.jpeg",
+    title: "Ash Longsleeve Men",
+    price: 28,
+  },
+
+  {
+    id: 3,
+    image: "images/blue.jpeg",
+    title: "Blue T-shirt Nike Air",
+    price: 17.8,
+  },
+
+  {
+    id: 4,
+    image: "images/R.jpeg",
+    title: "Basketball T-shirt Black",
+    price: 10,
+  },
+
+  {
+    id: 5,
+    image: "images/redtshirt.webp",
+    title: "Red T-shirt Nike Air",
+    price: 12,
+  },
+
+  {
+    id: 6,
+    image: "images/bluesnaeaker.jpeg",
+    title: "Blue Sneakers Men",
+    price: 45.8,
+  },
+
+  {
+    id: 7,
+    image: "images/ashsneakers.jpeg",
+    title: "Breathable Mesh Sneakers Men",
+    price: 49.5,
+  },
+
+  {
+    id: 8,
+    image: "images/blacknorange.webp",
+    title: "Black Nike Pro",
+    price: 33.9,
+  },
+];
